c7b564243e14421109275da138ee79f5a91985ea
matiasj899 <matiasjuarez899@gmail.com>
2022-06-27 17:15:08 -0300

    add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from './Pages/Login';
 
 
 
 function App() {
   return (
-    <div>
-     
-    </div>
+ 
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />}>
+      </Route>
+    </Routes>
+  </BrowserRouter>
+   
   );
 }
 
